19c67653ea8b5a9786665bd09f597aaef155660a
Marcin Gadek <marcingadek@yahoo.com>
2022-03-12 14:00:04 +0100

    Headless commented

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -98,9 +98,9 @@ export const config: WebdriverIO.Config = {
         maxInstances: 1,
         //
         browserName: 'chrome',
-          'goog:chromeOptions': {
-          args: ["--headless"]
-        },
+        //   'goog:chromeOptions': {
+        //   args: ["--headless"]
+        // },
         
         acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
